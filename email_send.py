83277986142ad16f23c194fb3ea9cde3c67c4aa9
fierce-adventurer <tripathiayush589@gmail.com>
2025-01-09 01:47:42 +0530

    initial commit

--- email_send.py
+++ email_send.py
@@ -1,12 +1,12 @@
 import smtplib
 
-to = input("enter the email of reciever")
-message = input("enter the message:-")
-sender_email= input("enter the sender email")
-passward = input("enter the passward")
+to = input("enter the email of reciever:  ")
+message = input("enter the message:-  ")
+sender_email= input("enter the sender email:  ")
+passward = input("enter the passward:  ")
 
 def sendEmail(to,message,sender_email,passward):
-    server = smtplib.SMTP('smtp.gmail.cpom',570)
+    server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
     server.login(sender_email,passward)
     server.sendmail(sender_email, to ,message)
